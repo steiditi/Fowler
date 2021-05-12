eb1c2e725033b2408b4f4e62f0dac4a0e561e906
steiditi <Timo.steidinger@gmx.de>
2021-05-12 09:05:47 +0200

    Improved Customer Class

--- src/Customer.java
+++ src/Customer.java
@@ -1,10 +1,8 @@
-
-import java.lang.*;
 import java.util.*;
 
-class Customer {
+public class Customer {
     private String name;
-    private Vector rentals = new Vector();
+    Vector <Rental> rentals = new Vector<>();
     public Customer (String newname){
         name = newname;
     };
@@ -16,54 +14,23 @@ class Customer {
     };
     public String statement() {
         double totalAmount = 0;
-        Enumeration enum_rentals = rentals.elements();
+        int frequentRenterPoints = 0;
+        Enumeration <Rental> enum_rentals = rentals.elements();
         String result = "Rental Record for " + this.getName() + "\n";
         result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
 
         while (enum_rentals.hasMoreElements()) {
             Rental each = (Rental) enum_rentals.nextElement();
-            //determine amounts for each line
-            //show figures for this rental
-            result += "\t" + each.getMovie().getTitle()+ "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(each.getCharge()) + "\n";
+            frequentRenterPoints += each.getFrequentRenterPoints();
+            result += "\t" + each.getMovie().getTitle()+ "\t" + "\t" + each.getDaysRented() + "\t"+ String.valueOf(each.getCharge()) + "\n";
+            totalAmount += each.getCharge();
         }
         //add footer lines
-        result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
-        result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) + " frequent renter points";
+        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+        result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
         return result;
     }
 
-//    It is worth stopping to think a bit about the last refactoring. Most refactorings reduce the amount
-//    of code, but this one increases it. That's because Java 1.1 requires a lot of statements to set up a
-//    summing loop. Even a simple summing loop with one line of code per element needs six lines of
-//    support around it. It's an idiom that is obvious to any programmer but is a lot of lines all the same.
-//    The other concern with this refactoring lies in performance. The old code executed the "while"
-//    loop once, the new code executes it three times. A while loop that takes a long time might impair
-//    performance. Many programmers would not do this refactoring simply for this reason. But note
-//    the words if and might. Until I profile I cannot tell how much time is needed for the loop to
-//    calculate or whether the loop is called often enough for it to affect the overall performance of the
-//    system. Don't worry about this while refactoring. When you optimize you will have to worry about
-//    it, but you will then be in a much better position to do something about it, and you will have more
-//    options to optimize effectively (see the discussion on page 69).
-
-    private double getTotalCharge() {
-        double result = 0;
-        Enumeration enum_rentals = rentals.elements();
-        while (enum_rentals.hasMoreElements()) {
-            Rental each = (Rental) enum_rentals.nextElement();
-            result += each.getCharge();
-        }
-        return result;
-    }
-
-    private int getTotalFrequentRenterPoints(){
-        int result = 0;
-        Enumeration enum_rentals = rentals.elements();
-        while (enum_rentals.hasMoreElements()) {
-            Rental each = (Rental) enum_rentals.nextElement();
-            result += each.getFrequentRenterPoints();
-        }
-        return result;
-    }
 
 }
     
